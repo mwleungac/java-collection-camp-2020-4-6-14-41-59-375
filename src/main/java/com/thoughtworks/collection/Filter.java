9ca278abe0820000dcf2794e1f94493925a52406
mwleungac <57056434+mwleungac@users.noreply.github.com>
2020-04-07 13:09:15 +0800

    Update Filter.java

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,31 +1,55 @@
 package com.thoughtworks.collection;
 
+
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
     List<Integer>  array;
 
+
     public Filter(List<Integer> array) {
      this.array = array;
+
+
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+
+        List<Integer> result = array.stream()
+                .filter(element -> element % 2 == 0).collect(Collectors.toList());
+        return result;
+
+        //throw new NotImplementedException();
     }
+
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        List<Integer> result = array.stream()
+                .filter(element -> element % 3 == 0).collect(Collectors.toList());
+
+        return result;
+        //throw new NotImplementedException();
     }
 
-    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList){
+
+        List<Integer> result = new ArrayList<Integer>(firstList);
+        result.retainAll(secondList);
+        return result;
+
+        //// result now contains only the elements which are contained in listA and listB.
+        //throw new NotImplementedException();
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List<Integer> result = array.stream()
+        .distinct().collect(Collectors.toList());
+        return result;
     }
 }
